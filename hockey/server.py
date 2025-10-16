713e7c3029bad6bd69087467983efe18447e2e21
Maksym Andriiaschuk <a.maksym0902@gmail.com>
2025-10-16 14:30:26 -0600

    fixed port

--- hockey/server.py
+++ hockey/server.py
@@ -79,4 +79,4 @@ def load_line():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
